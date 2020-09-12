7a926a90b26a37a11a77fc4d5ea1d77ece35779c
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-09-13 00:03:09 +0530

    Created an count app

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, {useState} from "react";
+import {View, Text,Button, StyleSheet} from "react-native";
 
-export default function App() {
-  return (
-    <View
-      style={{
+const styles = StyleSheet.create({
+    container: {
         flex: 1,
-        padding:20,
-        justifyContent: "flex-start",
-        alignItems: "flex-start",
-      }}
-    >
-      <Text>Ashfaq Nisar</Text>
-    </View>
-  );
+        padding: 20,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#000000"
+    }
+})
+const App = () => {
+    const [count, setCount] = useState(0);
+    const increaseTheCount = () => {
+        setCount(count + 1);
+    }
+    return (
+        <View
+            style={styles.container}
+        >
+            <Text style={{color: "white"}}>Ashfaq Nisar I am hello world </Text>
+            <Button style={{marginTop: 30, marginBottom: 30}} color={"#ed1c24"}
+                  onPress={increaseTheCount} title={"Increase"}/>
+            <Text style={{fontSize: 20, color: "#ed1c24"}}>Count is {count}</Text>
+        </View>
+    );
 }
+export default App
